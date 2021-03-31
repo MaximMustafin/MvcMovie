f207eee48b0810c602280d0a8ba4a9b048de85df
Maksim <maksim.mystafa@gmail.com>
2021-03-31 12:53:58 +0300

    deleted and moved old elements from monolite MvcMovie

--- MvcMovie/NLayerApp.WEB/Models/MovieGenreViewModel.cs
+++ MvcMovie/NLayerApp.WEB/Models/MovieGenreViewModel.cs
@@ -1,11 +1,12 @@
 using Microsoft.AspNetCore.Mvc.Rendering;
+using MvcMovie.NLayerApp.BLL.DTO;
 using System.Collections.Generic;
 
-namespace MvcMovie.Models
+namespace MvcMovie.NLayerApp.WEB.Models
 {
     public class MovieGenreViewModel
     {
-        public List<Movie> Movies { get; set; }
+        public List<MovieDTO> Movies { get; set; }
         public SelectList Genres { get; set; }
         public string MovieGenre { get; set; }
         public string SearchString { get; set; }
